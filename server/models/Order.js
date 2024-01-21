c52a6c909f7579749d2471a527da15dc13d6ae3b
Yana Burdakova <bu.asti@gmail.com>
2024-01-20 20:06:43 -0500

    feat: add Orders Schema

---
+++ server/models/Order.js
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+
+const OrderSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true
+        },
+        products: [
+            {
+                title: {
+                    type: String,
+                },
+                category: { 
+                    type: String,
+                },
+                quantity: {
+                    type: Number,
+                    default: 1
+                },
+                price: {
+                    type: Number,
+                },
+                measure: {
+                    type: String,
+                },
+            }
+        ],
+        amount: {
+            type: Number,
+            required: true
+        },
+        status: {
+            type: String,
+            default: "На рассмотрении"
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+export const Order = mongoose.model('Order', OrderSchema);
