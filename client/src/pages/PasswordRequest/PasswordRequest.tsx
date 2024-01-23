613d573e0c0804574a44b71a3bbd1540716c39c9
Yana Burdakova <bu.asti@gmail.com>
2024-01-23 12:59:56 -0500

    feat: add styles for Request password

--- client/src/pages/PasswordRequest/PasswordRequest.tsx
+++ client/src/pages/PasswordRequest/PasswordRequest.tsx
@@ -1,7 +1,11 @@
 import { Link } from 'react-router-dom'
+import { IoCheckmarkDoneSharp } from "react-icons/io5";
+
 import { CustomInput } from '../../components'
 import styles from './PasswordRequest.module.css'
 import { useState } from 'react';
+import axios from 'axios';
+import { BASE_URL } from '../../middleware/requestMethods';
 
 const PasswordRequest = () => {
   const [email, setEmail] = useState('');
@@ -9,7 +13,26 @@ const PasswordRequest = () => {
   const [phone, setPhone] = useState('');
   const [isSubmitted, setIsSubmitted] = useState(false);
   const [isSending, setIsSending] = useState(false);
-  
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  setIsSending(true);
+
+  try {
+    const response = await axios.post(`${BASE_URL}/request/send_request`, {
+      email,
+      username,
+      phone,
+    });
+    console.log(response.data);
+    setIsSubmitted(true);
+  } catch (error) {
+    console.error('Failed to send request', error);
+  }
+
+  setIsSending(false);
+  };
+
   return (
     <div className={styles.container}>
     <div className={styles.title}>
@@ -17,15 +40,42 @@ const PasswordRequest = () => {
     </div>
     <div className={styles.loginBox}>
       <div className={styles.p}>Введите Email клиента, имя и контактный телефон ответственного сотрудника</div>
-      <form>
-        <CustomInput type="email" label="Email" placeholder="Email" required getValue={setEmail}/>
-        <CustomInput type="text" label="Имя сотрудника" placeholder="Имя сотрудника" required getValue={setUsername}/>
-        <CustomInput type="phone" label="Номер телефона" placeholder="Номер телефона" isMask required getValue={setPhone}/>
-
-        <button className={styles.button}>Отправить запрос</button>
-        <Link to='/' className="">Вернуться на страницу авторизации</Link>
+      <form onSubmit={handleSubmit}>
+        <CustomInput 
+          type="email" 
+          label="Email" 
+          placeholder="Email" 
+          required 
+          getValue={setEmail}
+        />
+        <CustomInput 
+          type="text" 
+          label="Имя сотрудника" 
+          placeholder="Имя сотрудника" 
+          required 
+          getValue={setUsername}
+        />
+        <CustomInput 
+          type="phone" 
+          label="Номер телефона" 
+          placeholder="Номер телефона" 
+          isMask 
+          required 
+          getValue={setPhone}
+        />
+        <button className={styles.button} disabled={isSending}>Отправить запрос</button>
+        <Link to='/' className={styles.link}>Вернуться на страницу авторизации</Link>
       </form> 
     </div>
+    {isSubmitted &&
+      <div className={styles.successMessage}>
+        <p className={styles.text}>Спасибо!</p>
+        <p className={styles.text}> Запрос успешно отправлен. </p>
+        <p className={styles.text}>Администратор системы свяжется с вами в ближайшее время.</p>
+        <IoCheckmarkDoneSharp className={styles.icon}/>
+    </div>
+    }
+    
   </div>
   )
 }
