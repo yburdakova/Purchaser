67025f628dc4a0f9abb17500e525641f451333ea
Yana Burdakova <bu.asti@gmail.com>
2024-01-23 23:01:23 -0500

    feat: add breadcrumbs toHead

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,16 +1,28 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { FaPowerOff } from "react-icons/fa6";
 import { FaRegBell } from "react-icons/fa";
 
 
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { loginFinish } from '../redux/userRedux';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { menuLinks } from '../data/constants';
+import { RootState } from '../redux/store';
 
 const Header = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const location = useLocation();
+
+  const user = useSelector((state: RootState) => state.user.currentUser);
   const [notifyCounter, setNotifyCounter] = useState(0)
+  const [pageTitle, setPageTitle] = useState('Page Name');
+
+  useEffect(() => {
+    const currentPath = location.pathname.split('/').pop();
+    const currentMenuItem = menuLinks.find(link => link.path === currentPath);
+    setPageTitle(currentMenuItem ? currentMenuItem.title : 'Page Name');
+  }, [location]);
 
   const handleClickLogout = () => {
     dispatch(loginFinish());
@@ -19,16 +31,20 @@ const Header = () => {
 
   return (
     <header>
-      <div className="headerIcon" onClick={handleClickLogout}>
-        < FaRegBell />
-        <div className="notification">
-          {notifyCounter}
+      <div className="page_title">{user&&user.isAdmin ? "Администратор" : "Клиент"} / {pageTitle}</div>
+      <div className="header_icons">
+        <div className="headerIcon" onClick={handleClickLogout}>
+          < FaRegBell />
+          <div className="notification">
+            {notifyCounter}
+          </div>
+        </div>
+        
+        <div className="headerIcon" onClick={handleClickLogout}>
+          <FaPowerOff />
         </div>
       </div>
       
-      <div className="headerIcon" onClick={handleClickLogout}>
-        <FaPowerOff />
-      </div>
     </header>
   )
 }
--- client/src/pages/AdminDashboard.tsx
+++ client/src/pages/AdminDashboard.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const AdminDashboard = () => {
   return (
-    <div className='infopage'>adminDashboard</div>
+    <div className='infopage'>adminDashboard
+      <div className="widgetBox">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere consectetur labore totam quae. Sapiente expedita tenetur hic illum. Sapiente inventore explicabo voluptatem quia placeat dolorem, veniam debitis saepe iusto necessitatibus?</div>
+    </div>
   )
 }
 
