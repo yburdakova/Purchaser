7cc576c1a9882e157a8dfe567f2df5e19aad4000
Yana Burdakova <bu.asti@gmail.com>
2024-01-23 21:32:49 -0500

    feat: add interact elements to header and footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -4,12 +4,12 @@ import { FiPhone } from "react-icons/fi";
 const Footer = () => {
   return (
     <footer>
-      <div className="footer_contact">
-        <a href="tel:+1234567890" className="headerIcon shake">
+      <a className="footer_contact" href="tel:+1234567890">
+        <div className="headerIcon footerIcon shake">
           <FiPhone />
-        </a>
+        </div>
         <div className="">Связаться с администратором</div>
-      </div>
+      </a>
       <div className="">
         <div className="">
           © 2024 ООО "Поставка"
